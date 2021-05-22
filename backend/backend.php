71a271b01922cd8c35b4e8d0ea359f1b86ef7ca6
Rollaner <sebastianattoni@gmail.com>
2021-05-21 21:01:18 -0400

    comprobacion repositorio

--- backend/backend.php
+++ backend/backend.php
@@ -3,8 +3,6 @@ header('Content-type: application/json');
 header('Access-Control-Allow-Origin:*'):
 
 $guardar=file_get_contents('php://input');
-//$json=json_encode($notas);
-//$notas = json_decode($guardar);
 $archivo = fopen("notas.json","w");
 fwrite($archivo,$guardar);
 
